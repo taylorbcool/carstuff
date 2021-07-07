928e85a7db81669f8948ace3f0456ff9318ead16
Kip DeCastro <kipdec@Kips-MacBook-Air.local>
2021-07-06 23:57:30 -0600

    Driving with ghetto velocity kind of works

--- src/lua/carstuff.lua
+++ src/lua/carstuff.lua
@@ -1,10 +1,46 @@
+world_min = {
+  x = 0,
+  y = 5
+}
+
+world_max = {
+  x = 127,
+  y = 122
+}
+
+center = {
+  x = 127 /2,
+  y = 127 /2
+}
+
+level = {
+  x = 0,
+  y = 0
+}
+
+function drawLevel()
+  map(level.x, level.y, 0, 0, 128, 32)
+end
+
+cam = {
+  x = 0, 
+  y = 0
+}
+
+function drawCam()
+  camera(cam.x, cam.y)
+end
+
 actor = {
   coords = {
-    x = 15,
-    y = 30
+    x = 0,
+    y = 0
   },
   name = '',
-  sprite = 0
+  sprite = 0,
+  velocity = 0,
+  maxVelocity = 2,
+  direction = 90
 }
 
 function actor:new(o)
@@ -14,10 +50,22 @@ function actor:new(o)
   return o
 end
 
-function actor:move(coords)
-  self.coords.x = self.coords.x + coords.x
-  self.coords.y = self.coords.y + coords.y
+function worldCoordsToCoords(coords)
+  return {
+    x = coords.x,
+    y = coords.y + 5
+  }
 end
+
+function actor:move(coords)
+  self.coords.x = self.coords.x + (coords.x * self.velocity)
+  self.coords.y = self.coords.y + (coords.y * self.velocity)
+  return {
+    x = self.coords.x,
+    y = self.coords.y
+  }
+end
+
 
 function actor:draw()
 		-- print(self.coords.x..','..self.coords.y, self.coords.x, self.coords.y + 10)
@@ -41,6 +89,7 @@ function _init()
     actor:new({
       name = 'player',
       sprite = 1,
+      coords = center
     })
   }
 end
@@ -48,18 +97,28 @@ end
 function _update()
   local player = actors[1]
   local dir = { x = 0, y = 0}
-  if(btn(0)) dir = addcoords(dir, { x = -1, y = 0})
-  if(btn(1)) dir = addcoords(dir, { x = 1, y = 0})
-  if(btn(2)) dir = addcoords(dir, { x = 0, y = -1})
-  if(btn(3)) dir = addcoords(dir, { x = 0, y = 1})
-  --print(dir.x..','..dir.y,10,10)
-  player:move(dir)
+  if(btn(0)) then 
+    dir = addcoords(dir, { x = -1, y = 0})
+  end
+  if(btn(1)) then 
+    dir = addcoords(dir, { x = 1, y = 0})
+  end
+  if(btn(4)) then
+    dir = addcoords(dir, { x = 0, y = 1})
+    player.velocity = player.velocity + 0.1
+    if(player.velocity > player.maxVelocity) player.velocity = player.maxVelocity;
+  else 
+    player.velocity = player.velocity - 0.1
+    if(player.velocity < 0) player.velocity = 0
+  end
+  cam = addcoords(player:move(dir), { x = - 127 /2.0, y = -127 /2.0})
 end
 
 function _draw()
 		cls()
-    drawmap()
-	 for k,v in pairs(actors) do
+    drawCam()
+    drawLevel()
+	  for k,v in pairs(actors) do
     v:draw()
   end
 end
