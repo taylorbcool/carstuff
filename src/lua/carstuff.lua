59c1b61d81b21a6651911414cca840f87395619b
Taylor Cool <tcool@code-distillers.com>
2021-07-06 19:45:54 -0700

    steering things

--- src/lua/carstuff.lua
+++ src/lua/carstuff.lua
@@ -1,41 +1,45 @@
-Actor = {
+actor = {
   coords = {
-    x = 0,
-    y = 0
+    x = 15,
+    y = 30
   },
   name = '',
   sprite = 0
 }
 
-function Actor:new(o)
+function actor:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
-function Actor:move(coords)
+function actor:move(coords)
   self.coords.x = self.coords.x + coords.x
   self.coords.y = self.coords.y + coords.y
 end
 
-function Actor:draw()
-  --print(self.coords.x..','..self.coords.y, self.coords.x, self.coords.y + 10)
+function actor:draw()
+		-- print(self.coords.x..','..self.coords.y, self.coords.x, self.coords.y + 10)
   spr(self.sprite, self.coords.x, self.coords.y)
 end
 
-function AddCoords(c1, c2)
+function addcoords(c1, c2)
   return {
     x = c1.x + c2.x,
     y = c1.y + c2.y
   }
 end
 
+function drawmap()
+  map(0, 0, 0, 0, 32, 64)
+end
+
 function _init()
   -- track all the actors
   actors = {
-    Actor:new({
-      name = 'Player',
+    actor:new({
+      name = 'player',
       sprite = 1,
     })
   }
@@ -44,15 +48,17 @@ end
 function _update()
   local player = actors[1]
   local dir = { x = 0, y = 0}
-  if(btn(0)) dir = AddCoords(dir, { x = -1, y = 0})
-  if(btn(1)) dir = AddCoords(dir, { x = 1, y = 0})
-  if(btn(2)) dir = AddCoords(dir, { x = 0, y = -1})
-  if(btn(3)) dir = AddCoords(dir, { x = 0, y = 1})
+  if(btn(0)) dir = addcoords(dir, { x = -1, y = 0})
+  if(btn(1)) dir = addcoords(dir, { x = 1, y = 0})
+  if(btn(2)) dir = addcoords(dir, { x = 0, y = -1})
+  if(btn(3)) dir = addcoords(dir, { x = 0, y = 1})
+  --print(dir.x..','..dir.y,10,10)
   player:move(dir)
 end
 
 function _draw()
 		cls()
+    drawmap()
 	 for k,v in pairs(actors) do
     v:draw()
   end
